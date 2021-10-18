2c0d35c9adb0e19beec57c16a4ad29bb141cc077
emirkrbz <78754284+emirkrbz@users.noreply.github.com>
2021-10-19 00:02:13 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,6 @@
+let konutKredileri = ["Konut Kredileri", "Emlak Kredileri", "Kamu Konut Kredileri"];
+console.log("<ul>");
+for(let i = 0; i < konutKredileri.length; i++){
+    console.log("<li>"+ konutKredileri[i] + "</li>");
+}
+console.log("</ul>");
